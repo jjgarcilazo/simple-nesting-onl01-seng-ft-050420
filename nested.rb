b02c70d3f1c18bca4b68d8d454c187ebae6b7f1a
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-09 18:08:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,6 +89,7 @@ def changing_alan
  		{
         :grace_hopper => {
           :known_for => "COBOL",
+        end
        def changing_alan
           	programmer_hash =
           	programmer_hash =
@@ -112,7 +113,6 @@ def changing_alan
 
           		 programmer_hash[:alan_kay][:known_for] = alans_new_info
           		 programmer_hash
-             end
 
  def adding_to_dennis
    programmer_hash =
